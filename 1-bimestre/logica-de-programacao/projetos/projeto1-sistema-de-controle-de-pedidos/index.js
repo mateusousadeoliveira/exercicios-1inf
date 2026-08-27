56f071cc23f00587be9eec6374db603324e46acb
Mateus Sousa <129328490+mateusousadeoliveira@users.noreply.github.com>
2026-08-27 14:45:18 +0000

    calculo de sistema de pagamento do preojeto 01 adicionado

--- 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
+++ 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
@@ -1,5 +1,42 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
 
+const cliente = "Mariana Silva"
+const cidade = "Fortaleza"
+const produto = "Notebook Pro"
+const categoria = "Notebook"
+const preco = 3500
+const quantidade = 2
+const descontoPercentual = 10 
+const valorPago = 7000
+const subtotal = preco * quantidade
+const valorDesconto = subtotal * descontoPercentual/100
+const valorFinal = subtotal - valorDesconto
+const troco = valorPago - valorFinal
+const resumo = `==================
+TECHSTORE SISTEMAS
+RESUMO DO PEDIDO
+==================
+
+Cliente: ${cliente}
+Cidade: ${cidade}
+
+Produto: ${produto}
+Categoria: ${categoria}
+Quantidade: ${quantidade}
+Preço unitário: R$ ${preco}
+
+Subtotal: R$ ${subtotal}
+desconto: ${descontoPercentual}%
+Valor do desconto: R$ ${valorDesconto}
+Valor total: R$ ${valorFinal}
+
+Valor pago: R$ ${valorPago}
+Troco: R$ ${troco}
+
+Obrigado pela compra!
+======================
+`
+console.log(resumo)
 
 
 
