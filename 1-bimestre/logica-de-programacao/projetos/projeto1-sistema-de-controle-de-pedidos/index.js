6fae29c9d4b32977762dc4a61f2fe30b548f05e4
Mateus Sousa <129328490+mateusousadeoliveira@users.noreply.github.com>
2026-08-28 13:51:45 +0000

    correção no resumo e no console.log

--- 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
+++ 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
@@ -12,7 +12,10 @@ const subtotal = preco * quantidade
 const valorDesconto = subtotal * descontoPercentual/100
 const valorFinal = subtotal - valorDesconto
 const troco = valorPago - valorFinal
-const resumo = `==================
+const resumo = `${cliente} fez um pedio de um ${produto}, pagando R$ ${valorFinal} e recebendo R$ ${troco} de troco`
+
+console.log(resumo)
+console.log(`==================
 TECHSTORE SISTEMAS
 RESUMO DO PEDIDO
 ==================
@@ -34,10 +37,7 @@ Valor pago: R$ ${valorPago}
 Troco: R$ ${troco}
 
 Obrigado pela compra!
-======================
-`
-console.log(resumo)
-
+======================`)
 
 
 
