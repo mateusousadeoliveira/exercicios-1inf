af9ccfd00b8c33ab610295501f0d2006897b4b1f
Mateus Sousa <129328490+mateusousadeoliveira@users.noreply.github.com>
2026-09-19 16:34:27 -0300

    Add variables for nome, idade, and cidade

--- 3-bimestre/logica-de-programacao/aula08/index.js
+++ 3-bimestre/logica-de-programacao/aula08/index.js
@@ -1,10 +1,7 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
-
-
-
-
-
-
+const nome = "Carlos"
+const idade = 17
+const cidade = "Fortaleza"
 // === FIM DO CÓDIGO =======================
 // === NÃO FAZER NADA ABAIXO DESSA LINHA ===
 module.exports = { nome, idade, cidade }
