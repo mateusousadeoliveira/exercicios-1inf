8ff63866c6988a467bb7c26f9bafece51a0c3012
Mateus Sousa <129328490+mateusousadeoliveira@users.noreply.github.com>
2026-09-19 20:51:09 -0300

    Add files via upload

--- 3-bimestre/logica-de-programacao/aula17/index.js
+++ 3-bimestre/logica-de-programacao/aula17/index.js
@@ -1,9 +1,14 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
+const nota = 8
+let resultado
 
+if (nota >= 7){
+    resultado = "Aprovado"
+} else {
+    resultado = "Reprovado"
+}
 
-
-
-
+console.log(resultado)
 
 // === FIM DO CÓDIGO =======================
 // === NÃO FAZER NADA ABAIXO DESSA LINHA ===
